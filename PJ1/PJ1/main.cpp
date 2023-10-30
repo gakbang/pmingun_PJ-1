fc94509456d2fc7dddff8b562a9814ff1ad8c071
sm-amoled <moktak072@naver.com>
2023-10-30 14:24:16 +0900

    xcode이긴한데,,, 일단 올려볼게용

---
+++ PJ1/PJ1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PJ1
+//
+//  Created by Park Sungmin on 10/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
